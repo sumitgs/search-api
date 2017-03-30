4f79f495373dde9855b3adc3358b8ef54ba5e9b8
Sumit Suthar <sumitgajjar02@gmail.com>
2017-03-30 20:14:09 +0530

    removed config file and related function

--- main.go
+++ main.go
@@ -5,26 +5,14 @@ import (
 	"log"
 	"net/http"
 
-	"encoding/json"
 	"os"
 
 	"github.com/search-api/api"
 )
 
-type Cfg struct {
-	Port string
-}
-
 func main() {
-
-	file, _ := os.Open("conf.json")
-	decoder := json.NewDecoder(file)
-	cfg := Cfg{}
-	err := decoder.Decode(&cfg)
-	if err != nil {
-		fmt.Println("error: ", err.Error())
-	}
-
 	http.HandleFunc("/", api.Search())
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", cfg.Port), nil))
+
+	port := os.Getenv("PORT")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
